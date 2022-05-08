db9924e5879132821177e2c934c62fc3376bf769
Dr Efficient <67394745+sudo455@users.noreply.github.com>
2022-05-08 18:44:08 +0300

    shell short name fix and finale

--- sorts.py
+++ sorts.py
@@ -1,5 +1,7 @@
-def bubblesort(numbers_un, time):
-    
+from time import time
+
+
+def bubblesort(numbers_un):
     numbers = numbers_un.copy()
 
     start = time()
@@ -11,8 +13,46 @@ def bubblesort(numbers_un, time):
 
     end = time()
 
-    print("Time for sorting bubblesort style:", end - start, "\n")
-    return numbers
+    total_time = end - start
 
-def quickshort(numbers, time):
-    print("comming soon")
+    return total_time
+
+
+def bubblesort_number_sorting(numbers_unsorted):
+    numbers = numbers_unsorted.copy()
+
+    for i in range(len(numbers)):
+        for j in range(len(numbers) - 1):
+            if numbers[j] > numbers[j + 1]:
+                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+
+
+def shell_sort(numbers_unsorted):
+    numbers = numbers_unsorted.copy()
+
+    start = time()
+
+    table_length = len(numbers_unsorted)
+
+    gap = len(numbers_unsorted) // 2
+
+    while gap > 0:
+
+        for i in range(gap, table_length):
+
+            temp = numbers[i]
+            j = i
+
+            while j >= gap and numbers[j - gap] > temp:
+                numbers[j] = numbers[j - gap]
+                j -= gap
+
+            numbers[j] = temp
+        gap = gap // 2
+
+    end = time()
+    print("list: ", numbers)
+
+    total_time = end - start
+
+    return total_time
