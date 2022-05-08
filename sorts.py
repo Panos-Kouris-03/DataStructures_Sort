970b04d90c00356ff06b81184d2437f6e5a015c3
Dr Efficient <67394745+sudo455@users.noreply.github.com>
2022-05-08 21:40:29 +0300

    heap sort done

--- sorts.py
+++ sorts.py
@@ -53,6 +53,37 @@ def shell_sort(numbers_unsorted):
     end = time()
     print("list: ", numbers)
 
-    total_time = end - start
+    return end - start
 
-    return total_time
+
+def heap_sort(numbers_unsorted):
+    numbers_to_sort = numbers_unsorted.copy()
+
+    table_length = len(numbers_unsorted)
+
+    time_start = time()
+
+    def thinks_in_to_place(table_array, interger, tl):
+        start = 2 * interger
+        end = (2 * interger) + 1
+
+        if start <= tl and table_array[start] > table_array[interger]:
+            largest = start
+        else:
+            largest = interger
+        if end < tl and table_array[end] < table_array[largest]:
+            largest = end
+        if largest != interger:
+            temp = table_array[interger]
+            table_array[interger] = table_array[largest]
+            table_array[largest] = temp
+
+    for i in range(table_length // 2 - 1, -1, -1):
+        thinks_in_to_place(numbers_to_sort, i, table_length)
+
+    for i in range(table_length - 1, 0, -1):
+        numbers_to_sort[i], numbers_to_sort[0] = numbers_to_sort[0], numbers_to_sort[i]
+        thinks_in_to_place(numbers_to_sort, i, table_length)
+
+    time_stop = time()
+    return time_stop - time_start
